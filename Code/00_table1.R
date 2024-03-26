955617b429ee50fe343abfbbd8a0aa4dd4917cc9
Justin Naidu <justinnaidu@Justins-MacBook-Pro-3.local>
2024-03-26 11:33:45 -0400

    added table1 R file to 'Code' folder

---
+++ Code/00_table1.R
@@ -0,0 +1,37 @@
+library(dplyr)
+library(gtsummary)
+library(here)
+
+here::i_am("Code/00_table1.R")
+absolute_path <- here::here('Raw_data/f75_interim.csv')
+data <- read.csv(absolute_path, header = TRUE)
+
+# Generating Table 1
+table1 <- data %>%
+  select(agemons, sex, weight, arm, muac) %>%
+  rename(Age = agemons,
+         Sex = sex,
+         Weight = weight,
+         Arm = arm,
+         MUAC = muac) %>%
+  gtsummary::tbl_summary(by = Arm,
+                         statistic = list(Age ~ "{mean} ({sd})",
+                                          Weight ~ "{mean} ({sd})",
+                                          MUAC ~ "{mean} ({sd})"),
+                         missing = "no") %>%
+  bold_labels() %>% 
+  italicize_levels() %>% 
+  add_n() %>% 
+  add_overall() %>% 
+  add_p() %>% 
+  bold_p(t=0.8) %>% 
+  modify_caption("**Table 1. Patient Characteristics**")
+
+# Printing Table 1
+print(table1)
+
+# Save table1 as an RDS file
+saveRDS(
+  data, 
+  file = here::here("Output/table1.rds")
+)
